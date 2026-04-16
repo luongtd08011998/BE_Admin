91d125180e093fef439b2440cd044d93e336e36c
Tran Duc Luong <luongtd08011998@gmail.com>
2026-04-17 00:40:20 +0700

    update hay qua

--- src/main/java/vn/hoidanit/springrestwithai/config/SecurityConfig.java
+++ src/main/java/vn/hoidanit/springrestwithai/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:4173",
+                "http://localhost:3000", "http://localhost:3001", "http://localhost:3003", "http://localhost:4173",
                 "http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://127.0.0.1:3002", "http://127.0.0.1:4173",
                 "http://125.253.121.171", "http://125.253.121.171:3000", "http://localhost:3001",
                 "https://125.253.121.171", "https://125.253.121.171:3000", "https://125.253.121.171:3001"));
